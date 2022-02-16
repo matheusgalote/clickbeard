a79bf3e67ce5824b239e470c71eff693daec39be
Trauvin <mateusgalote@gmail.com>
2022-02-16 10:03:22 -0300

    admin login system

--- php-admin/authenticate.php
+++ php-admin/authenticate.php
@@ -4,7 +4,7 @@ session_start();
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
-$DATABASE_NAME = 'phplogin';
+$DATABASE_NAME = 'phpadmin';
 
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno() ) {
@@ -30,7 +30,7 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?'
       $_SESSION['loggedin'] = TRUE;
       $_SESSION['name'] = $_POST['username'];
       $_SESSION['id'] = $id;
-      header('Location: home.php');
+      header('Location: ../dashboard.html');
     } else {
       echo 'Senha e/ou Usuário incorretos';
     }
